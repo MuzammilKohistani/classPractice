51f00a3575d26b1c8aba41d6c09a60af3152f3f1
muzokoh <muzamilkohistani20117@gmail.com>
2022-05-18 17:38:23 +0300

    isler gucler

---
+++ src/PatatesEdecemKamili.java
@@ -0,0 +1,16 @@
+
+public class PatatesEdecemKamili {
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("patates edecem kamili haberi yok");
+		System.out.println("havar komsular havar komsular ahmet'imi patates etmisler ");
+		
+		
+		
+		
+
+	}
+
+}
