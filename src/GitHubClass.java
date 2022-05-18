725ed72bc1c4d375905720de8e3d6ee47be7c19e
muzokoh <muzamilkohistani20117@gmail.com>
2022-05-18 14:16:41 +0300

    I changed

--- src/GitHubClass.java
+++ src/GitHubClass.java
@@ -1,4 +1,21 @@
 
 public class GitHubClass {
 
+	
+	public static void main(String[] args) {
+		
+		
+		int a = 10 ;
+		int b = 10 ;
+		int c = 10 ;
+		System.out.println( a+b+c );
+		
+		
+		
+		
+	}
+	
+	
+	
+	
 }
